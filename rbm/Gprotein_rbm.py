c9ec9458a3e955fea2059ce027ac8eb6310c1204
Matteo Bonamassa <matteo.bonamassa1@gmail.com>
2020-04-07 17:27:44 +0200

    Add for on temperature

--- rbm/Gprotein_rbm.py
+++ rbm/Gprotein_rbm.py
@@ -28,65 +28,66 @@ spec.loader.exec_module(Gprotein_util)
 
 be.set_seed(137) # for determinism
 
-### Load Data
-dataset_path = os.path.join(os.path.dirname(__file__), "../dataset/")
-train_file = "sim-120_0T" # Name of the .dat train file in the dataset dir
+out_file = open("energy-vs-var-fanatasy-cmap.dat", "a+")
+for temperature in os.listdir("../dataset"):
 
-train_patterns_list = []
-with open(dataset_path+train_file+'.dat') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=' ')
-    for i,row in enumerate(readCSV):
-        if (i%2==0):
-            row=list(map(int,row[:-1])) #exclude last space character
-            train_patterns_list.append(row)
+    dataset_path = "../dataset/"
+    train_file = temperature # Name of the .dat train file in the dataset dir
 
-samples = np.asarray(train_patterns_list)
-###
+    train_patterns_list = []
+    with open(dataset_path+train_file) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=' ')
+        for i,row in enumerate(readCSV):
+            if (i%2==0):
+                row=list(map(int,row[:-1])) #exclude last space character
+                train_patterns_list.append(row)
 
-def run(num_epochs=1, show_plot=False):
-    num_hidden_units = 1540
-    batch_size = 100
-    mc_steps = 1
-    beta_std = 0.6
-    train_fraction = 0.8
+    samples = np.asarray(train_patterns_list)
 
-    # set up the reader to get minibatches
-    with batch.in_memory_batch(samples, batch_size, train_fraction) as data:
+    def run(num_epochs=100, show_plot=False):
+        num_hidden_units = 1540
+        batch_size = 100
+        mc_steps = 10
+        beta_std = 0.6
+        train_fraction = 0.8
 
-        # set up the model and initialize the parameters
-        vis_layer = layers.BernoulliLayer(data.ncols)
-        hid_layer = layers.BernoulliLayer(num_hidden_units, center=False)
+        # set up the reader to get minibatches
+        with batch.in_memory_batch(samples, batch_size, train_fraction) as data:
 
-        rbm = BoltzmannMachine([vis_layer, hid_layer])
-        rbm.connections[0].weights.add_penalty({'matrix': pen.l2_penalty(0.001)})
-        rbm.initialize(data, method='hinton')
+            # set up the model and initialize the parameters
+            vis_layer = layers.BernoulliLayer(data.ncols)
+            hid_layer = layers.BernoulliLayer(num_hidden_units, center=False)
 
-        print('training with persistent contrastive divergence')
-        cd = fit.SGD(rbm, data)
+            rbm = BoltzmannMachine([vis_layer, hid_layer])
+            rbm.connections[0].weights.add_penalty({'matrix': pen.l2_penalty(0.001)})
+            rbm.initialize(data, method='hinton')
 
-        learning_rate = schedules.PowerLawDecay(initial=0.01, coefficient=0.1)
-        opt = optimizers.ADAM(stepsize=learning_rate)
+            print('training with persistent contrastive divergence')
+            cd = fit.SGD(rbm, data)
 
-        cd.train(opt, num_epochs, mcsteps=mc_steps, method=fit.pcd)
-#        Gprotein_util.show_metrics(rbm, cd.monitor)
+            learning_rate = schedules.PowerLawDecay(initial=0.01, coefficient=0.1)
+            opt = optimizers.ADAM(stepsize=learning_rate)
 
-    return rbm
+            cd.train(opt, num_epochs, mcsteps=mc_steps, method=fit.pcd, verbose=False)
+    #        Gprotein_util.show_metrics(rbm, cd.monitor)
 
-if __name__ == "__main__":
+        return rbm
 
-    rbm = run(show_plot = False)
-    print("DONE!")
-"""
-    n_fantasy = 100
-    fantasy_steps = 100
-    beta_std = 0.6
-    run_mean_field = True
+    if __name__ == "__main__":
+#        print(temperature)
+        rbm = run(show_plot = False)
+#        print("DONE!")
 
-    fantasy_particles = Gprotein_util.compute_fantasy_particles(rbm, n_fantasy, fantasy_steps,beta_std=beta_std,run_mean_field=run_mean_field)
+        n_fantasy = 1000
+        fantasy_steps = 100
+#        beta_std = 0.6
+#        run_mean_field = True
+#        print("computing fp")
+        fantasy_particles = Gprotein_util.compute_fantasy_particles(rbm, n_fantasy, fantasy_steps)
 
-    FP = fantasy_particles.sum(2)/1.540
-    av_E = FP[:,fantasy_steps-1].sum()/n_fantasy
-    av_E2 = (np.square(FP[:,fantasy_steps-1])).sum()/n_fantasy
-    var = av_E2 - av_E**2
-    print("Mean E:\t",av_E,"\t",var)
-"""
+        FP = fantasy_particles.sum(2)/1540
+        av_E = FP[:,fantasy_steps-1].sum()/n_fantasy
+        av_E2 = (np.square(FP[:,fantasy_steps-1])).sum()/n_fantasy
+        var = av_E2 - av_E**2
+#        print("Mean E:\t",av_E,"\t",var)
+        out_file.write(temperature+" "+ str(av_E) + " " + str(var)+"\n")
